23c861a8b0d3edb9cf029e5f6b6a3eeb3c950d22
Hande Kara <handekarai.hk@gmail.com>
2022-07-15 17:46:00 +0300

    change condition case to successCodeWithNoReturnObject in delete methods

--- Charger/Charger/Presentation/Appointments/Model/AppointmentsModel.swift
+++ Charger/Charger/Presentation/Appointments/Model/AppointmentsModel.swift
@@ -10,9 +10,10 @@ import Foundation
 class AppointmentsModel: HTTPClient {
     
     // delete specific appointment
-    // my network layer returns decode error when there are success code 200 and no object returns from network
-    // for that reason implemented delegation pattern in failure part with decode error
-    // it means delete appointment succesfully done, if something returns with 200 code it would be nice
+    /* my network layer returns successCodeWithNoReturnObject error
+     when there are success code 200 and no object returns from network
+     for that reason implemented delegation pattern in failure part with successCodeWithNoReturnObject error
+     it means delete appointment succesfully done, if something returns with 200 code it would be nice */
     func deleteAppointment(_ completion: @escaping (Result<Appointment,RequestError>)-> Void) async {
         let endpoint = DeleteAppointmentEndpoint()
         let result =  await sendRequest(endpoint: endpoint, responseModel: Appointment.self)
